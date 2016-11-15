e0248c8ca8f7143f64b9037fd89bc937d758ac74
Georg Bakken Idland <georg.idland@skat.dk>
2016-11-15 09:24:38 +0100

    added dotenv for .env files updated gitignore updated package.json

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 var express = require('express')
 var bodyParser = require('body-parser')
 var execFile = require('child_process').exec;
@@ -9,42 +11,37 @@ var app = express()
 var jsonParser = bodyParser.json()
 
 // create application/x-www-form-urlencoded parser
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
-
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
-
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+var urlencodedParser = bodyParser.urlencoded({
+    extended: false
 })
 
 
-app.post('/deploy', urlencodedParser, function(req,res) {
+app.listen(3000, function() {
+    console.log('Example app listening on port 3000!')
+})
 
-  res.status(200).send('hej ' + req.body.user_name);
-  console.log(req.body);
-  var indexStaging = req.body.text.indexOf('staging');
-      
-  if(indexStaging !== -1) {
-   console.log("staging exists!");
-   //var branch = req.body.text.substring(indexStaging+8);
-   var arrayText = req.body.text.split(' ');
+app.get('/', function(req, res) {
+    res.send('Hello World!')
+})
 
-   console.log(arrayText);
-   execFile('./deploy.sh', function(error, stdout, stderr) {
-        if( error )
-        {
+
+app.post('/deploy-staging', urlencodedParser, function(req, res) {
+
+    if (req.body.token == process.env.SLACK_TOKEN) {
+        res.status(200).send('Got it ' + req.body.user_name);
+        console.log(req.body);
+    }
+
+    //var branch = req.body.text.substring(indexStaging+8);
+    var arrayText = req.body.text.split(' ');
+
+    console.log(arrayText);
+    execFile('./deploy.sh', function(error, stdout, stderr) {
+        if (error) {
             console.log(error)
         }
-	console.log(stdout);
+        console.log(stdout);
     });
+}
 
-   
-
-
-  }
- 
 });
- 
