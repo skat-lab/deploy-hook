4046a13d68c7e72e584bfc9c120f5096cbf6ee27
Ubuntu <it@tolderfonen-frontend-dev.oqgpk1eidh0etafg4mcj1nanhh.fx.internal.cloudapp.net>
2016-11-17 12:35:32 +0000

    updated deploy script

--- deploy-frontend.sh
+++ deploy-frontend.sh
@@ -4,9 +4,10 @@ BRANCH="$1"
 REPO="$2"
 PROJECT_DIRECTORY="$REPO-$BRANCH"
 
+
 if [ ! -d "$PROJECT_DIRECTORY" ]; then
   # Control will enter here if $PROJECT_DIRECTORY doesn't exist.
-  git clone git@github.com:$REPOS.git "$PROJECT_DIRECTORY"
+  git clone git@github.com:$REPO.git "$PROJECT_DIRECTORY"
 fi
 
 cd "$PROJECT_DIRECTORY"
@@ -14,13 +15,14 @@ cd "$PROJECT_DIRECTORY"
 if [ `git branch -r --list origin/$BRANCH ` ]
 then
   git checkout $BRANCH
-  echo "Hej hej"
-
+  
   cp config/nginx.staging.conf config/nginx.conf
 
   #Deployment tasks
   docker-compose up -d
+  sleep 2m
   docker-compose run -d node bower install --allow-root
+  sleep 1m
   docker-compose run -d node gulp --type=staging
 else
   echo "$BRANCH does not exist"
