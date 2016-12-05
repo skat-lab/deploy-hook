ba654d9b35a3aafc818d52292a2162548586b1a7
Georg Idland <georg.idland@skat.dk>
2016-12-05 10:20:09 +0000

    Updated deploy script

--- deploy-frontend.sh
+++ deploy-frontend.sh
@@ -15,12 +15,14 @@ cd "$PROJECT_DIRECTORY"
 if [ `git branch -r --list origin/$BRANCH ` ]
 then
   git checkout $BRANCH
-  
+  git pull
+ 
   cp config/nginx.staging.conf config/nginx.conf
+  docker kill $(docker ps -q)
 
   #Deployment tasks
   docker-compose up -d
-  sleep 2m
+  sleep 30s
   docker-compose run -d node bower install --allow-root
   sleep 1m
   docker-compose run -d node gulp --type=staging
